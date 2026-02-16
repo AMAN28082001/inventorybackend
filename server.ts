40b35d4b54669f127fa68140f4b6f3330046b79e
AMAN28082001 <amankrrajak288@gmail.com>
2026-02-16 13:16:34 +0530

    Update allowed origins in CORS middleware to replace a local development URL for improved access control

--- server.ts
+++ server.ts
@@ -35,7 +35,7 @@ const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || ['http://localhost:3000', 'http://localhost:3050', 'http://localhost:3001', 'http://43.204.133.228:3051', 'http://43.204.133.228:3050', 'http://localhost:3002', 'http://localhost:3003','http://quotation.chairbordsolar.com', 'http://api.inventory.chairbordsolar.com', 'https://api.inventory.chairbordsolar.com', 'https://inventory.chairbordsolar.com', 'http://43.204.133.228:3052','https://quotation.chairbordsolar.com','http://192.168.1.47:3000','http://192.0.0.2:3000'];
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || ['http://localhost:3000', 'http://localhost:3050', 'http://localhost:3001', 'http://43.204.133.228:3051', 'http://43.204.133.228:3050', 'http://localhost:3002', 'http://localhost:3003','http://quotation.chairbordsolar.com', 'http://api.inventory.chairbordsolar.com', 'https://api.inventory.chairbordsolar.com', 'https://inventory.chairbordsolar.com', 'http://43.204.133.228:3052','https://quotation.chairbordsolar.com','http://192.168.1.25:3000','http://192.0.0.2:3000'];
 app.use(cors({
   origin: (origin, callback) => {
     // Allow requests with no origin (like mobile apps or curl requests)
